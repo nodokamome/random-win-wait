60c79ab6f12d1f3ff7c07fcb87fb4057b11fa40a
nodokamome <mail@nodokamome.com>
2022-02-05 23:34:00 +0900

    add SEO

---
+++ next-sitemap.js
@@ -0,0 +1,5 @@
+module.exports = {
+  siteUrl: 'https://random.nodokamome.com',
+  generateRobotsTxt: true,  // robots.txtを作成するか
+}
+
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,52 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
+import { DefaultSeo, NextSeo } from 'next-seo'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo
+        defaultTitle='当たり待ち'
+        canonical={process.env.URL}
+        description='当たるまで待つだけの確率シミュレーション'
+        twitter={{
+          handle: '@nodokamome',
+          site: '@nodokamome',
+          cardType: 'summary',
+        }}
+        openGraph={{
+          type: 'website',
+          title: '当たり待ち',
+          description: '当たるまで待つだけの確率シミュレーション',
+          site_name: '当たり待ち',
+          url: `${process.env.URL}`,
+          // TODO OGP画像
+          images: [
+            {
+              url: `${process.env.URL}/ogp.png`,
+              width: 800,
+              height: 600,
+              alt: '当たり待ちのOGP画像',
+            }],
+        }}
+      />
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.GA_MEASUREMENT_ID}');
+          `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
